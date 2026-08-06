1affe3038254918a9fbfbe87aae18c849f94c76e
Ethamukkala Yugandhar <104614963+yugandhar99@users.noreply.github.com>
2026-08-06 15:32:32 -0500

    Update autoPull.sh

--- .github/workflows/autoPull.sh
+++ .github/workflows/autoPull.sh
@@ -5,7 +5,7 @@ git config --global user.email "yugandharethamukkala1999@gmail.com"
 # Set GITHUB_TOKEN environment variable
 # Do not store GitHub tokens in git config. Use a credential manager or GitHub CLI authentication.
 git config credential.helper store
-
+ 
 git status
 git pull
 echo 'Pleas Wait , Loading files from github...'
