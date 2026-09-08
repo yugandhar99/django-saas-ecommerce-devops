f4c7bba9e86b7333d12d8b4ddb034a9a44c14232
Ethamukkala Yugandhar <104614963+yugandhar99@users.noreply.github.com>
2026-09-08 16:34:04 -0500

    Update autoPull.sh

--- .github/workflows/autoPull.sh
+++ .github/workflows/autoPull.sh
@@ -1,6 +1,6 @@
 # Basc Script to Pull New Update from the rep
 
-git config --global user.name "yugandhar99" 
+git config --global user.name "yugandhar99"
 git config --global user.email "yugandharethamukkala1999@gmail.com"
 # Set GITHUB_TOKEN environment variable
 # Do not store GitHub tokens in git config. Use a credential manager or GitHub CLI authentication.
